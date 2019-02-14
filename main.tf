e27322d7232076ce9cab5d6726d9791d76c3650c
Igor Rodionov <goruha@gmail.com>
2019-02-15 02:39:10 +0600

    Added iam role

---
+++ main.tf
@@ -0,0 +1,92 @@
+module "label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
+  namespace  = "${var.namespace}"
+  stage      = "${var.stage}"
+  name       = "${var.name}"
+  delimiter  = "${var.delimiter}"
+  attributes = "${var.attributes}"
+  tags       = "${var.tags}"
+}
+
+module "kops_metadata" {
+  source       = "git::https://github.com/cloudposse/terraform-aws-kops-metadata.git?ref=tags/0.1.1"
+  dns_zone     = "${var.cluster_name}"
+  masters_name = "${var.masters_name}"
+  nodes_name   = "${var.nodes_name}"
+}
+
+resource "aws_iam_role" "default" {
+  name        = "${module.label.id}"
+  description = "Role that can be assumed to access chamber"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        "${module.kops_metadata.masters_role_arn}",
+        "${module.kops_metadata.nodes_role_arn}",
+      ]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "default" {
+  role       = "${aws_iam_role.default.name}"
+  policy_arn = "${aws_iam_policy.default.arn}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_policy" "default" {
+  name        = "${module.label.id}"
+  description = "Grant permissions for chamber"
+  policy      = "${data.aws_iam_policy_document.default.json}"
+}
+
+data "aws_route53_zone" "default" {
+  name         = "${var.cluster_name}."
+  private_zone = false
+}
+
+
+data "aws_iam_policy_document" "default" {
+  statement {
+    actions   = ["ssm:DescribeParameters"]
+    resources = ["*"]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions   = ["${var.ssm_actions}"]
+    resources = ["${var.ssm_resources}"]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions   = ["kms:Decrypt"]
+    resources = ["${var.kms_key_arn}"]
+    effect    = "Allow"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,66 @@
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "name" {
+  type        = "string"
+  default     = "external-dns"
+  description = "Name (e.g. `external-dns`)"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map(`Cluster`,`us-east-1.cloudposse.com`)"
+}
+
+variable "cluster_name" {
+  type        = "string"
+  description = "Kops cluster name (e.g. `us-east-1.cloudposse.com` or `cluster-1.cloudposse.com`)"
+}
+
+variable "masters_name" {
+  type        = "string"
+  default     = "masters"
+  description = "Kops masters subdomain name in the cluster DNS zone"
+}
+
+variable "nodes_name" {
+  type        = "string"
+  default     = "nodes"
+  description = "Kops nodes subdomain name in the cluster DNS zone"
+}
+
+variable "kms_key_arn" {
+  description = "ARN of the KMS key which will decrypt secret strings"
+}
+
+variable "ssm_actions" {
+  type        = "list"
+  default     = ["ssm:GetParametersByPath", "ssm:GetParameters"]
+  description = "Actions to allow in the policy"
+}
+
+variable "ssm_resources" {
+  type        = "list"
+  default     = ["*"]
+  description = "Resources to apply the actions specified in the policy"
+}
